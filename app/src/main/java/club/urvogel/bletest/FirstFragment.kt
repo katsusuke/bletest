5ff8cf02102db17469f3b18361c50b8d651b72c6
Katsusuke Shimizu <katsusuke@gmail.com>
2020-12-05 19:28:36 +0900

    check permissions

--- app/src/main/java/club/urvogel/bletest/FirstFragment.kt
+++ app/src/main/java/club/urvogel/bletest/FirstFragment.kt
@@ -1,5 +1,6 @@
 package club.urvogel.bletest
 
+import android.Manifest
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothManager
 import android.content.Context
@@ -7,17 +8,29 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.view.*
+import android.webkit.PermissionRequest
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.fondesa.kpermissions.allGranted
+import com.fondesa.kpermissions.anyPermanentlyDenied
+import com.fondesa.kpermissions.extension.permissionsBuilder
 import com.google.android.material.snackbar.Snackbar
 import no.nordicsemi.android.support.v18.scanner.*
-
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class FirstFragment : Fragment() {
+    private val request by lazy {
+        permissionsBuilder(
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.BLUETOOTH_ADMIN,
+            Manifest.permission.BLUETOOTH
+        ).build()
+    }
+
     lateinit var listView: ListView
     lateinit var arrayAdapter: ArrayAdapter<Device>
 
@@ -95,6 +108,21 @@ class FirstFragment : Fragment() {
     }
 
     fun onScanning() {
+        request.removeAllListeners()
+        request.addListener {
+            when {
+                it.anyPermanentlyDenied() -> Toast.makeText(
+                    requireContext(),
+                    "NO PERMISSON",
+                    Toast.LENGTH_SHORT
+                ).show()
+                it.allGranted() -> doScan()
+            }
+        }
+        request.send()
+    }
+
+    fun doScan() {
         try {
             arrayAdapter.clear()
             mList.addDevices(getBluetoothAdapter().getBondedDevices())
@@ -105,12 +133,13 @@ class FirstFragment : Fragment() {
                 .setUseHardwareBatchingIfSupported(false).build()
             val filters = ArrayList<ScanFilter>()
             mScanner.startScan(filters, settings, mScanCallback)
+            Handler(Looper.getMainLooper()).postDelayed({
+                mScanner.stopScan(mScanCallback)
+            }, 3000)
+
         } catch (ex: Exception) {
             ex.printStackTrace()
             return
         }
-        Handler(Looper.getMainLooper()).postDelayed({
-            mScanner.stopScan(mScanCallback)
-        }, 3000)
     }
 }
