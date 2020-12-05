f6b54f973d51ebce65e557e47ca4e363b758c500
Katsusuke Shimizu <katsusuke@gmail.com>
2020-12-05 16:26:23 +0900

    initial commit

---
+++ app/src/main/java/club/urvogel/bletest/FirstFragment.kt
@@ -0,0 +1,36 @@
+package club.urvogel.bletest
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import androidx.fragment.app.Fragment
+
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class FirstFragment : Fragment() {
+    lateinit var listView: ListView
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_first, container, false)
+        listView = view.findViewById(R.id.listView)
+        val texts = arrayOf(
+            "abc ", "bcd", "cde", "def", "efg",
+            "fgh", "ghi", "hij", "ijk", "jkl",
+            "klm"
+        )
+        val arrayAdapter = ArrayAdapter(view.context, android.R.layout.simple_list_item_1, texts)
+        listView.adapter = arrayAdapter
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
